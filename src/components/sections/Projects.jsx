f8e5c44d55f6bcdd175e9b3ff43b96f84ac492c4
ch-focus <charles@edpassare.com>
2025-08-17 19:02:29 +0100

    initial commit

---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,45 @@
+export const Projects = () => {
+    return ( 
+    <section
+     id="projects" 
+     className="min-h-screen flex items-center justify-center py-20"
+    >
+        <div className="max-w-5xl mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 t0-cyan-400 bg-clip-text text-transparent text-center">
+                {" "}
+                 Featured Projects
+                 </h2>
+                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                    <div>
+                        <h3> Color Picker </h3>
+                        <p>
+                           A color picker project allows users to select colors, often for 
+                           use in design or other applications. It typically provides a 
+                           visual interface to navigate through colors, with options to specify 
+                           them using RGB, HSV, or hexadecimal codes.
+                        </p>
+                        <div>
+                            {["React", "Node.js", "Javascript", "HTML"].map((tech, key) => (
+                                <span
+                            key={key}
+                              className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                            hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition 
+                            "
+                            >
+                                {tech}
+
+                            </span>
+                            ))}
+                        </div>
+
+                        <div>
+                            <a href="https://github.com/ch-focus/color-picker.git"> View Project </a>
+                        </div>
+                    </div>
+
+                 </div>
+        </div>
+    </section>
+    );
+};
