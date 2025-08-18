060b81bb43d4f9420d94673e2c6054e9d2ab3e95
ch-focus <charles@edpassare.com>
2025-08-18 20:09:19 +0100

    added new file

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -18,7 +18,7 @@ export const Projects = () => {
                            visual interface to navigate through colors, with options to specify 
                            them using RGB, HSV, or hexadecimal codes.
                         </p>
-                        <div>
+                        <div className="flex flex-wrap gap-2 mb-4"> 
                             {["React", "Node.js", "Javascript", "HTML"].map((tech, key) => (
                                 <span
                             key={key}
@@ -32,8 +32,47 @@ export const Projects = () => {
                             ))}
                         </div>
 
-                        <div>
-                            <a href="https://github.com/ch-focus/color-picker.git" className="text-blue-400 hover:text-blue-300 transition-colors"> View Project </a>
+                        <div className="flex justify-between items-center">
+                            <a 
+                              href="https://github.com/ch-focus/color-picker.git"
+                              className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                              >
+                                {" "}
+                                 View Project  {" "} 
+                                 </a>
+                        </div>
+                    </div>
+
+                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition ">
+                        <h3 className="text-xl font-bold mb-2"> AI VERIFY </h3>
+                        <p className="text-gray-400 mb-3">
+                           AI verification tool designed to detect and verify the authenticity, bias,
+                           and accuracy of AI-generated content. Using a combination of machine learning
+                           models, natural language processing, and computer vision, the system analyzes text, 
+                           images, and videos to flag potential misinformation, deepfakes, or biased outputs.
+                        </p>
+                        <div className="flex flex-wrap gap-2 mb-4"> 
+                            {["React", "Node.js", "Javascript", "Tailwind"].map((tech, key) => (
+                                <span
+                            key={key}
+                              className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                            hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all 
+                            "
+                            >
+                                {tech}
+
+                            </span>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-between items-center">
+                            <a 
+                              href="#"
+                              className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                              >
+                                {" "}
+                                 View Project  {" "} 
+                                 </a>
                         </div>
                     </div>
 
